e9e5627acd5c18a72eca200985b8560b2e9d0cb9
rikhsi <rikhsimboyev1997@gmail.com>
2026-04-13 15:08:06 +0500

    fix: smth

--- src/app/pages/loan/components/calculator-form/calculator-form.ts
+++ src/app/pages/loan/components/calculator-form/calculator-form.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TranslocoDirective } from '@jsverse/transloco';
 import { NzRadioComponent, NzRadioGroupComponent } from 'ng-zorro-antd/radio';
 import { InputSlider, LabelControlSecondary } from '@shared/components';
 
 @Component({
   selector: 'cf-calculator-form',
-  imports: [InputSlider, LabelControlSecondary, NzRadioComponent, NzRadioGroupComponent, FormsModule],
+  imports: [InputSlider, LabelControlSecondary, NzRadioComponent, NzRadioGroupComponent, FormsModule, TranslocoDirective],
   templateUrl: './calculator-form.html',
   styleUrl: './calculator-form.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/loan/components/product-acception/product-acception.ts
+++ src/app/pages/loan/components/product-acception/product-acception.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzCheckboxComponent } from 'ng-zorro-antd/checkbox';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'cf-product-acception',
-  imports: [NzCheckboxComponent, NzButtonComponent, NzIconDirective],
+  imports: [NzCheckboxComponent, NzButtonComponent, NzIconDirective, TranslocoDirective],
   templateUrl: './product-acception.html',
   styleUrl: './product-acception.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
