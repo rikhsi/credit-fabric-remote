07c07b0b0511d28957c38afc992cc43d8bc88a9f
Omonbek <omonbek.rikhsimboev@eventplatform.ru>
2026-05-19 12:15:50 +0500

    fix: fixes

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,12 +1,24 @@
+import { inject } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from 'src/environments/development';
+import { LocalStorageService } from '@core/services';
+import { LocalStorageItem } from '@constants';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const accessToken: string = environment.proxyKey;
+  const storageService = inject(LocalStorageService);
+  const tokenService = inject(JwtHelperService);
+
+  const accessToken: string = storageService.getItem(LocalStorageItem.AccessToken);
+
+  const isExpired = tokenService.isTokenExpired(accessToken);
+
+  if (isExpired) {
+    return next(req);
+  }
 
   const authReq = req.clone({
     setHeaders: {
-      Authorization: `${accessToken}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 
