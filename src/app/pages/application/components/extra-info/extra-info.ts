86fd289849147b08698cc22052e7be1c7f4ff132
Omonbek Rikhsimboyev <104456857+rikhsi@users.noreply.github.com>
2026-06-02 14:50:57 +0500

    fix: fixes

--- src/app/pages/application/components/extra-info/extra-info.ts
+++ src/app/pages/application/components/extra-info/extra-info.ts
@@ -2,14 +2,13 @@ import { ChangeDetectionStrategy, Component, input, output } from '@angular/core
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
-import { NzTypographyComponent } from 'ng-zorro-antd/typography';
 import { Card } from '@shared/components';
 import { FlowExtraInformationForm } from '@pages/application/models/form';
 import { ExtraInfoItem } from '@pages/application/components/extra-info/components';
 
 @Component({
   selector: 'cf-extra-info',
-  imports: [Card, NzButtonComponent, NzIconDirective, NzTypographyComponent, TranslocoDirective, ExtraInfoItem],
+  imports: [Card, NzButtonComponent, NzIconDirective, TranslocoDirective, ExtraInfoItem],
   templateUrl: './extra-info.html',
   styleUrl: './extra-info.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
