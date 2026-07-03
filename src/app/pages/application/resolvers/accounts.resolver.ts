b70b5a097d2acd7d07fe1b64c0cd72971fa9e421
Omonbek Rikhsimboyev <rikhsimboyev1997@gmail.com>
2026-07-03 12:51:37 +0500

    fix: fixes

--- src/app/pages/application/resolvers/accounts.resolver.ts
+++ src/app/pages/application/resolvers/accounts.resolver.ts
@@ -1,13 +1,20 @@
 import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
-import { catchError, of } from 'rxjs';
+import { ResolveFn, Router, UrlTree } from '@angular/router';
+import { catchError, EMPTY } from 'rxjs';
 import { OnlineApiService } from '@api/controllers/los';
 import { OnlineAccount } from '@api/models/los/account';
+import { LoanRoute, RootRoute } from '@app/constants/route-path';
 
-export const accountsResolver: ResolveFn<OnlineAccount[]> = (route) => {
+export const accountsResolver: ResolveFn<OnlineAccount[] | UrlTree> = (route) => {
   const api = inject(OnlineApiService);
+  const router = inject(Router);
 
   const applicationId = route.params['applicationId'];
 
-  return api.getAccounts$(applicationId).pipe(catchError(() => of<OnlineAccount[]>([])));
+  return api.getAccounts$(applicationId).pipe(
+    catchError(() => {
+      void router.navigate(['/', RootRoute.Loan, LoanRoute.List]);
+      return EMPTY;
+    }),
+  );
 };
